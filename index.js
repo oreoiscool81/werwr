8289a8bbe9b77a94e7e6c45d925140a3beabb21d
oreoiscool81 <sigma1234670@gmail.com>
2025-01-11 14:36:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🐟 Fisching in The Depths", "🐟 Fisching in Ancient Isles"];
+const statusMessages = ["🦕fighting seabeast"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
